21771f10d45412f934aece50ec3cb720579b15a1
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-01-07 14:43:28 +0900

    問題を解いた

---
+++ AOJ/ITP/6_A.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+#define max(a, b) ((a) > (b) ? (a) : (b)) // 大きい方を返す
+
+#include <iomanip>
+#include <stdio.h>
+
+#define for_(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor_(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define rep(i, n) for_(i, 0, n)
+//  rep(i, 10)cout << i << endl;
+// for 文を短くする
+#define rrep(i, n) rfor_(i, 0, n)
+// for を大きい方から実行する
+
+typedef long long ll;
+
+int main() {
+  int a[1002];
+  int n;
+  cin >> n;
+    rep(i, n) cin >> a[i];
+    rrep(i, n){
+      cout << a[i];
+      if (i ) cout << " ";
+
+     }
+    cout << endl;
+      return 0;
+}
