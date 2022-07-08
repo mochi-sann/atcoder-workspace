bb5baade5da727e90b2b2e1de927733d5a15a3bb
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-07-09 08:57:52 +0900

    add abc049

---
+++ abc049/A/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+
+using namespace std;
+#define max(a, b) ((a) > (b) ? (a) : (b)) // 大きい方を返す
+
+#define for_(i, a, b) for (int i = (a); i < (b); ++i)
+#define for_i(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rfor_(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define rep(i, n) for_(i, 0, n)
+//  rep(i, 10)cout << i << endl;
+// for 文を短くする
+#define rrep(i, n) rfor_(i, 0, n)
+// for を大きい方から実行する
+
+typedef long long ll;
+
+int main() {
+  ll a;
+  cin >> a;
+
+  cout << a << endl;
+  rrep(i, -100) { cout << i << endl; }
+  return 0;
+}
